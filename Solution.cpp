5297b391e0ee198dc816d88945f19063f7ac3c00
Caleb <ccheuk02@student.ubc.ca>
2024-08-02 13:38:52 -0600

    Added Solution.cpp and Solution.h

---
+++ Solution.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Froys on 02/08/2024.
+//
+
+#include "Solution.h"
+#include <vector>
+
+using namespace std;
+
+void rref(vector<int>& matrix) {
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+}
---
+++ Solution.h
@@ -0,0 +1,20 @@
+//
+// Created by Froys on 02/08/2024.
+//
+
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    void rref(vector<int>& matrix);
+    vector<int> ldiv(vector<int>& p1, vector<int>& p2);
+};
+
+
+
+#endif //SOLUTION_H
